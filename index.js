27625622a2fac1c9e8610b2c21fe6f6e695755ba
omnarayansingh <om.singh.5211@gmail.com>
2023-02-03 16:53:50 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+import express, { response } from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
+
+const uri = "mongodb+srv://om:1234om@cluster0.dztj6bc.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+    
+const userSchema = new mongoose.Schema({
+      name: String,
+      email: String,
+      password: String
+  })
+  
+const User = new mongoose.model("User", userSchema)
+  
+
+app.post("/login", (req, res)=> {
+      const { email, password} = req.body
+      User.findOne({ email: req.body.email}, (err, user) => {
+          if(user){
+              if(password === user.password ) {
+                  res.send({message: "Login Successfull", user: user})
+              } else {
+                  res.send({ message: "Password didn't match"})
+              }
+          } else {
+              res.send({message: "User not registered"})
+          }
+      })
+  }) 
+
+  app.post("/register", async(req, res)=> {
+      const { name, email, password} = req.body
+      let user = await User.findOne({email: req.body.email })
+      console.log(user)  
+      if(user){
+        res.send({message: "User already registerd"})
+        } else {
+        user = new User({
+            name,
+            email,
+            password
+        })
+        await user.save()
+        res.send({message: "successfuly register login now"})
+       
+    }
+    })  
+  
+  app.listen(9002,() => {
+      console.log("BE started at port 9002")
+  })
